5403a9bf3695ed63a848a6b529bf12ad24a6b958
Charlie-Gauvin <gauvinch9@gmail.com>
2025-01-08 16:46:10 +0100

    feature : Integrate Modal + Responsive design OK

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import GameButton from "./components/GameButton";
+import RulesModal from "./components/RulesModal";
 
 function App() {
   return (
     <div className="min-h-screen bg-radial-game font-barlow pt-24">
       <Header  />
       <GameButton />
+      <RulesModal />
     </div>
   );
 }
